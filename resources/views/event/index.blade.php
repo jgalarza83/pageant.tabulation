403e2f68344e94a4bd3b8c4fbd3f8db8cd5d8c3d
Jorge Ivan Galarza <jorge_galarza83@hotmail.com>
2024-11-12 22:44:16 +0800

    Added images to Events

--- resources/views/event/index.blade.php
+++ resources/views/event/index.blade.php
@@ -1,28 +1,27 @@
 @php
    $photos = [
        // https://placehold.co/300x600 -- NULL
-       'photogenic' => 'photogenic.png',
-       'congeniality' => null,
-       'people choice' => null,
-       'creative jeans' => 'creative-jeans.png',
-       'production' => 'production.png',
-       'sport' => 'sport-attire.png',
-       'evening' => 'evening-gown.png',
-       'picture analysis' => null,
+       'photogenic' => 'photogenic.jpg',
+       'congeniality' => 'congeniality.jpg',
+       'people choice' => 'people-choice.jpg',
+       'creative jeans' => 'jeans.jpg',
+       'production' => 'production.jpg',
+       'sport' => 'sport.jpg',
+       'evening' => 'evening.jpg',
+       'picture analysis' => 'picture-analysis.jpg',
    ];
 @endphp
 @extends('layouts.app')
 
 @section('content')
    <x-header-title class="my-10">Events</x-header-title>
-   <div class="grid grid-cols-6 px-24 gap-10 mb-8">
+   <div class="grid xl:grid-cols-6 max-lg:grid-cols-4 px-24 gap-10">
       @foreach ($events as $event)
          <x-category
             title="{{ ucwords($event->name) }}"
-            image="{{ $photos[$event->name] == null ? 'https://placehold.co/300x600' : asset('/img/' . $photos[$event->name]) }}"
+            image="{{ $photos[$event->name] == null ? 'https://placehold.co/300x500' : asset('/img/events/' . $photos[$event->name]) }}"
             link="{{ route('event.contestants', $event->id) }}"
          />
-
       @endforeach
    </div>
 @endsection
